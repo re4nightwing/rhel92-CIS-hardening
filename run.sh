5db5c84a9ac63a4c32c94b3a8de8fff4f45bda33
re4nightwing <dulan9595531@gmail.com>
2024-01-21 01:40:25 +0530

    scripts added to the run file

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # List of scripts to be executed
-SCRIPTS=("content_rule_sshd_set_keepalive.sh")
+SCRIPTS=("content_rule_accounts_password_pam_retry.sh" "content_rule_accounts_password_set_max_life_existing.sh" "content_rule_accounts_password_set_min_life_existing.sh" "content_rule_audit_rules_privileged_commands.sh" "content_rule_disable_host_auth.sh" "content_rule_service_nfs_disabled.sh" "content_rule_service_nftables_disabled.sh" "content_rule_service_rpcbind_disabled.sh" "content_rule_sshd_set_keepalive.sh" "generate_grub2_password.sh" )
 
 # Loop through each script
 for SCRIPT in "${SCRIPTS[@]}"; do
